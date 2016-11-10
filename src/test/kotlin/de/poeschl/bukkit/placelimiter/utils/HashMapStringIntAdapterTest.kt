3ebbfb7def99f6a0f87fbc33d6eefb3a06171b89
Poeschl <poeschl_markus@web.de>
2016-11-10 12:24:52 +0100

    Moshi adapter tests and ignored tests because of static bukkit calls

---
+++ src/test/kotlin/de/poeschl/bukkit/placelimiter/utils/HashMapStringIntAdapterTest.kt
@@ -0,0 +1,37 @@
+package de.poeschl.bukkit.placelimiter.utils
+
+import org.assertj.core.api.Assertions
+import org.junit.Test
+
+class HashMapStringIntAdapterTest {
+
+    @Test
+    fun testHashMapFromJson() {
+        val dummyJson = listOf("STONE|0", "DIRT:2|2")
+        val shouldResult = hashMapOf(Pair("STONE", 0), Pair("DIRT:2", 2))
+
+        //WHEN
+        val testAdapter = HashMapStringIntAdapter()
+
+        //THEN
+        val fromJsonResult = testAdapter.hashMapFromJson(dummyJson)
+
+        //VERIFY
+        Assertions.assertThat(fromJsonResult).isEqualTo(shouldResult)
+    }
+
+    @Test
+    fun testHashMapToJson() {
+        val dummyMap = hashMapOf(Pair("STONE", 0), Pair("DIRT:2", 2))
+        val shouldJson = listOf("STONE|0", "DIRT:2|2")
+
+        //WHEN
+        val testAdapter = HashMapStringIntAdapter()
+
+        //THEN
+        val toJsonResult = testAdapter.hashMapToJson(dummyMap)
+
+        //VERIFY
+        Assertions.assertThat(toJsonResult).isEqualTo(shouldJson)
+    }
+}
---
+++ src/test/kotlin/de/poeschl/bukkit/placelimiter/utils/WorldJsonAdapterTest.kt
@@ -0,0 +1,44 @@
+package de.poeschl.bukkit.placelimiter.utils
+
+import com.nhaarman.mockito_kotlin.mock
+import org.assertj.core.api.Assertions
+import org.bukkit.World
+import org.junit.Ignore
+import org.junit.Test
+
+//TODO: Utilize powermock to mock the static Bukkit calls
+class WorldJsonAdapterTest {
+
+    @Ignore
+    @Test
+    fun testPlayerFromJson() {
+        val dummyName = "Overworld"
+        val shouldWorld: World = mock()
+
+        //WHEN
+        val testAdapter = WorldJsonAdapter()
+
+        //THEN
+        val fromJsonResult = testAdapter.worldFromJson(dummyName)
+
+        //VERIFY
+        Assertions.assertThat(fromJsonResult).isEqualTo(shouldWorld)
+    }
+
+    @Ignore
+    @Test
+    fun testHashMapToJson() {
+        val dummyWorld: World = mock()
+        val resultName = "Overworld"
+
+        //WHEN
+        val testAdapter = WorldJsonAdapter()
+
+        //THEN
+        val fromJsonResult = testAdapter.worldToJson(dummyWorld)
+
+        //VERIFY
+        Assertions.assertThat(fromJsonResult).isEqualTo(resultName)
+    }
+
+}
